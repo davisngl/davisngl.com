d1146b7a360e8eb6a45326575b580f280d514f94
Dāvis Naglis <davisnaglis2@gmail.com>
2023-10-20 00:43:04 +0300

    make the layout wider

--- resources/js/Layouts/Main.vue
+++ resources/js/Layouts/Main.vue
@@ -3,7 +3,7 @@ import Navigation from '@/Components/Navigation.vue'
 </script>
 
 <template>
-    <main class="mx-auto w-full px-7 leading-relaxed sm:px-10 md:max-w-4xl md:leading-9">
+    <main class="mx-auto w-full px-7 leading-relaxed sm:px-10 md:max-w-6xl md:leading-9">
         <Navigation class="mt-10" />
 
         <section :class="[['Project', 'Projects'].includes($page.component) ? 'mt-10 md:mt-20' : ' mt-8 md:mt-15']">
